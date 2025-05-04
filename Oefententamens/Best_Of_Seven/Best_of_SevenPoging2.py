23709b025b002ee4dff09564a9d76b0145a9a01d
Lambour <lambour_maarten@hotmail.com>
2025-05-04 11:27:10 +0200

    Wijziging

---
+++ Oefententamens/Best_Of_Seven/Best_of_SevenPoging2.py
@@ -0,0 +1,25 @@
+from Oefententamens.UitwerkingenDocent.bestofseven import gewonnen_team1
+
+print(f"Maarten Lambour Spring School 7123479")
+
+naam_team_A = str(input("Voer de naam van het eerste team in: "))
+naam_team_B = str(input("Voer de naam van het tweede team in: "))
+
+punten_team_A = []
+punten_team_B = []
+
+eriseenwinnaar = False
+aantal_wedstrijden = 0
+
+while eriseenwinnaar == False:
+    gewonnen_team1 = 0
+    gewonnen_team2 = 0
+    aantal_wedstrijden = aantal_wedstrijden + 1
+    print()
+    print("Uitslag wedstrijd", wedstrijdnr)
+
+    puntenteam1 = int(input("   Aantal punten " + naamteam1 + ": "))
+    puntenteam2 = int(input("   Aantal punten " + naamteam2 + ": "))
+
+    puntenteam1array.append(puntenteam1)
+    puntenteam2array.append(puntenteam2)
